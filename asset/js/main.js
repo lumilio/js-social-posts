776be09e3f8016765023d52825c18fedd83805e9
lumilio <logistic-division@lumilium.com>
2021-10-25 21:24:48 +0200

    inizio esercizio

---
+++ asset/js/main.js
@@ -0,0 +1,32 @@
+
+
+
+
+/* Creare una pagina che elenchi una lists di post socials a partire da un array di oggetti.
+
+
+Gli attributi minimi del modello di un post: id, contenuto, immagine,
+autore (nome, avatar), numero di likes, data creazione.
+
+
+Un secondo array conterrà solo gli id dei posts a cui abbiamo dato like.
+
+
+Immagini: va bene utilizzare qualsiasi servizio di placeholder
+ad es. Unsplash (https://unsplash.it/300/300?image=<id>)
+
+
+
+BONUS:
+Bonus 1: Date formattate come tempo relativo (es. "due mesi fa")
+Bonus 2: Gestire l'assenza dell'immagine profilo con un elemento di fallback che contiene le iniziali dell'utente (es. Sofia Perlari > SP)
+Bonus 3: Click al pulsante "Mi Piace" incrementa il counter dei like al post. */
+ 
+
+
+
+
+console.log('ciao');
+
+
+
